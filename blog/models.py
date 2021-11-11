75942ca4df4157a93b5347611a4cd629b2bfda27
Bóris Pelichek Bueno <66342335+BorisPelichek@users.noreply.github.com>
2021-11-11 12:54:57 -0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 
 
 class Post(model.Model):
-    nome = forms.CharField(label='Nome', max_length=100)
-    assunto = forms.CharField(label='Assunto', max_length=100)
-    email = forms.EmailField(label='E-mail', max_length=100)
-    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+    nome = models.CharField(label='Nome', max_length=100)
+    assunto = models.CharField(label='Assunto', max_length=100)
+    email = models.EmailField(label='E-mail', max_length=100)
+    mensagem = models.CharField(label='Mensagem', widget=forms.Textarea())
     data_post = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
