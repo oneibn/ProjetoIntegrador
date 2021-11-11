0d699f48aec74e4417e76864a3db6b34e0220a8c
Bóris Pelichek Bueno <66342335+BorisPelichek@users.noreply.github.com>
2021-11-10 23:10:29 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
 
-# Create your views here.
+from .models import Post
+
+
+# Utilizei function class based views, talvez mudar para class based views
+
+def index(request):
+    context = {
+        'post': Post.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+def envio(request):
+    context = {
+         
+    }
+    return render(request, 'envio.html', context)
+  
+  
+def golpes(request):
+    context = {
+         
+    }
+    return render(request, 'golpes.html', context)
+  
+  
+def dicas(request):
+    context = {
+         
+    }
+    return render(request, 'dicas.html', context)
+
