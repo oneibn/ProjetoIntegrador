4db476608cba2e0db5e31811ce6cc03f569297dc
Bóris Pelichek Bueno <66342335+BorisPelichek@users.noreply.github.com>
2021-11-11 15:27:07 -0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(model.Model):
     nome = models.CharField(label='Nome', max_length=100)
     assunto = models.CharField(label='Assunto', max_length=100)
     email = models.EmailField(label='E-mail', max_length=100)
-    mensagem = models.CharField(label='Mensagem', widget=forms.Textarea())
+    mensagem = models.TextField(label='Mensagem')
     data_post = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
