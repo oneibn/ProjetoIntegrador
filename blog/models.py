2c8a3506e0a3b6a6625ce2989a7953f56175da96
Bóris Pelichek Bueno <66342335+BorisPelichek@users.noreply.github.com>
2021-11-10 22:14:03 -0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(model.Model):
+    nome = forms.CharField(label='Nome', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+    data_post = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.assunto
