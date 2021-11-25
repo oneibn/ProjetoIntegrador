6c4ceddb8549a5c8efa1701188053c1fd941b59a
Bóris Pelichek Bueno <66342335+BorisPelichek@users.noreply.github.com>
2021-11-24 21:41:10 -0300

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class Comentarios(forms.Form):
   
-    nome = forms.CharField(label='Nome', max_length=100)
-    assunto = forms.CharField(label='Assunto', max_length=100)
+    nome = forms.CharField(label='Nome e Sobrenome', max_length=100)
     email = forms.EmailField(label='E-mail', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=100)
     mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
