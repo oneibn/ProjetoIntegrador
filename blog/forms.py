10a637e2623802fc5f70d3e12912f9ea4d93623c
Bóris Pelichek Bueno <66342335+BorisPelichek@users.noreply.github.com>
2021-11-24 21:50:35 -0300

    Delete forms.py

--- blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class Comentarios(forms.Form):
-  
-    nome = forms.CharField(label='Nome e Sobrenome', max_length=100)
-    email = forms.EmailField(label='E-mail', max_length=100)
-    assunto = forms.CharField(label='Assunto', max_length=100)
-    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
