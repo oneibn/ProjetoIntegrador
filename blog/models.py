9c0dde65acc9df29187c368ab66192d171faa319
Onei de Barros Neto <onei.neto@gmail.com>
2021-11-25 00:04:07 -0300

    funcionando

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    nome = models.CharField(label='Nome', max_length=100)
-    assunto = models.CharField(label='Assunto', max_length=100)
-    email = models.EmailField(label='E-mail', max_length=100)
-    mensagem = models.TextField(label='Mensagem')
+    nome = models.CharField(max_length=100)
+    assunto = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    mensagem = models.TextField(max_length=1000)
     data_post = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
