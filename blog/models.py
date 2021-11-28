dca1b69ff561a9949b55addc257f06f82ff60998
Onei de Barros Neto <onei.neto@gmail.com>
2021-11-28 11:01:05 -0300

    salvando no banco de dados OK

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,17 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    postId = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
     assunto = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     mensagem = models.TextField(max_length=1000)
     data_post = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.assunto
+class Envio(models.Model):
+    envioId = models.AutoField(primary_key=True)
+    nomesobrenome = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+    mensagem = models.CharField(max_length=1000)
+
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect
 
-from .models import Post
+from .models import Post, Envio
 
 # Utilizei function class based views, talvez mudar para class based views
 
@@ -12,11 +12,19 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-
+## Aqui a view recebe as informações do formulário por HttpPost, cria uma instância e salva a instância no banco de dados
 def envio(request):
-  
+    if request.method == 'POST':
+        nomesobrenome = request.POST['nomesobrenome']
+        email = request.POST['email']
+        telefone = request.POST['telefone']
+        mensagem = request.POST['mensagem']
+
+        ins = Envio(nomesobrenome=nomesobrenome, email=email, telefone=telefone, mensagem=mensagem)
+        ins.save()
+
     context = {
-         
+        'envio': Envio.objects.all()
     }
     return render(request, 'envio.html', context)
   
